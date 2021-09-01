ab1d1748c3eb57c894428b78e444d1116dde8d3e
totw5701 <76473868+totw5701@users.noreply.github.com>
2021-09-02 00:16:45 +0900

    spring boot start
    spring, lombok

---
+++ src/main/java/com/example/restaurantrecommend/RestaurantRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.example.restaurantrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantRecommendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantRecommendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/restaurantrecommend/RestaurantRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restaurantrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestaurantRecommendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
